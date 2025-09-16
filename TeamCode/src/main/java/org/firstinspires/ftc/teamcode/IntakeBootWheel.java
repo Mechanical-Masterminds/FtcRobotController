2330b3ea7c51cbeae1b560fe1c4c7d5f95703cbf
Mechanical Masterminds <mechanicalmastermindsftc@gmail.com>
2025-09-16 15:50:24 -0400

    Fixed program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeBootWheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeBootWheel.java
@@ -1,12 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+@TeleOp(name = "Intake")
 public class IntakeBootWheel extends LinearOpMode{
     private DcMotor intakeMotor;
+    @Override
     public void runOpMode() throws InterruptedException {
         intakeMotor = hardwareMap.dcMotor.get("INTAKE");
 
@@ -15,7 +18,7 @@ public class IntakeBootWheel extends LinearOpMode{
 
         while (opModeIsActive()) {
             double rightStickX = gamepad1.right_stick_x;
-            intakeMotor.setPower(Math.min(Math.max(0,rightStickX),1));
+            intakeMotor.setPower(gamepad1.left_stick_x);
             telemetry.addData("Power of intake:", intakeMotor.getPower());
             telemetry.update();
         }
