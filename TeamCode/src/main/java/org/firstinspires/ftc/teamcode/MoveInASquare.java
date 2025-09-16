11d7761e3ebdb5931c5e57ae317f3ba5e8f8adef
mechanicalmastermindsftc <mechanicalmastermindsftc@gmail.com>
2025-09-16 17:46:25 -0400

    Move in A square
    We made some code using pedro pathing to make our robot move in a square, each side being 24 inches(Not done yet!)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MoveInASquare.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
+import com.pedropathing.
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+@Autonomous
+public class MoveInASquare extends LinearOpMode {
+
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+        Follower follower =  Constants.createFollower(hardwareMap,hardwareMap);
+        
+        while(opModeIsActive() && !isStopRequested()) {
+            path = follower.pathBuilder()
+                    .addPath(new BezierLine(scorePose, pickup1Pose))
+                    .setLinearHeadingInterpolation(scorePose.getHeading(), pickup1Pose.getHeading())
+                    .addPath(new BezierLine(pickup1Pose, scorePose))
+                    .setLinearHeadingInterpolation(pickup1Pose.getHeading(), scorePose.getHeading())
+                    .build();
+            follower.followPath(path);
+        }
+
+    }
+}
